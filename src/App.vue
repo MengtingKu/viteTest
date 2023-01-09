79b96f55919d56308854c123ab9537b6ef48502f
Ku <polorbear520@gmail.com>
2023-01-09 14:14:06 +0800

    update4

--- src/App.vue
+++ src/App.vue
@@ -8,14 +8,14 @@ import HelloWorld from './components/HelloWorld.vue'
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="You did it! 測試成功" />
+      <HelloWorld msg="You did it! TEST \0.0/" />
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
       </nav>
     </div>
-  </header>
+  </header>git
 
   <RouterView />
 </template>
